7db7f19fc71874361fe78ef3c106a69f157da72f
Just_Ho_it <zalop3128@kakao.com>
2024-04-10 16:36:35 +0900

    Initial Settings

---
+++ src/main/java/com/example/springoauth2clientjwt/SpringOauth2ClientJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springoauth2clientjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ClientJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOauth2ClientJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springoauth2clientjwt/SpringOauth2ClientJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springoauth2clientjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ClientJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
