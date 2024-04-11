76830864f892b6e03aa1810b247badd9edacb91b
Just_Ho_it <zalop3128@kakao.com>
2024-04-11 21:42:05 +0900

    Oauth2 JWT - JWT 발급 구현

--- src/main/java/com/example/springoauth2clientjwt/config/SecurityConfig.java
+++ src/main/java/com/example/springoauth2clientjwt/config/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.example.springoauth2clientjwt.config;
 
 
+import com.example.springoauth2clientjwt.jwt.JWTUtil;
+import com.example.springoauth2clientjwt.oauth2.CustomSuccessHandler;
 import com.example.springoauth2clientjwt.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,6 +18,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final JWTUtil jwtUtil;
+    private final CustomSuccessHandler customSuccessHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -37,7 +40,8 @@ public class SecurityConfig {
         httpSecurity
                 .oauth2Login((oauth2) -> oauth2
                         .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
-                                .userService(customOAuth2UserService)));
+                                .userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler));
 
         //경로별 인가 작업
         httpSecurity
